6c0e02b2c6dc7ef6d9d0f80963d13436e90cecc3
almartin82 <almartin@gmail.com>
2025-12-31 20:11:17 -0500

    Add Python wrapper package

---
+++ pycaschooldata/__init__.py
@@ -0,0 +1,16 @@
+"""
+pycaschooldata - Python wrapper for California school enrollment data.
+
+Thin rpy2 wrapper around the caschooldata R package.
+Returns pandas DataFrames.
+"""
+
+from .core import (
+    fetch_enr,
+    fetch_enr_multi,
+    tidy_enr,
+    get_available_years,
+)
+
+__version__ = "0.1.0"
+__all__ = ["fetch_enr", "fetch_enr_multi", "tidy_enr", "get_available_years"]
